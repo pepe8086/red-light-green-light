c192bf769403c4e7b1f7ad4ca1cc9b3ee25017f1
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-18 19:31:18 +0200

    default code for player

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
-input.onButtonPressed(Button.A, function () {
-    state = GREENLIGHT
-    basic.showIcon(IconNames.Yes)
-})
-input.onButtonPressed(Button.B, function () {
-    state = REDLIGHT
-    basic.showIcon(IconNames.No)
-})
-let state = 0
+let movement = 0
 let REDLIGHT = 0
+let state = 0
 let GREENLIGHT = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    state = receivedNumber
+})
 GREENLIGHT = 1
 REDLIGHT = 2
 radio.setGroup(1)
 basic.forever(function () {
-    radio.sendNumber(state)
+    if (state == GREENLIGHT) {
+        basic.showIcon(IconNames.Yes)
+    } else if (state == REDLIGHT) {
+        basic.showIcon(IconNames.No)
+    }
+})
+basic.forever(function () {
+    if (state == REDLIGHT) {
+        movement = Math.abs(input.acceleration(Dimension.Strength) - 1000)
+        if (movement != 0) {
+            game.gameOver()
+        }
+    }
 })
