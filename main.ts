4fff19b34c96966e2d612757e51ff1816c895a86
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-19 01:41:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     state = GREENLIGHT
     basic.showIcon(IconNames.Yes)
+    music.playMelody("E C D C E D C D ", 329)
 })
 input.onButtonPressed(Button.B, function () {
     state = REDLIGHT
@@ -9,6 +10,7 @@ input.onButtonPressed(Button.B, function () {
 let state = 0
 let REDLIGHT = 0
 let GREENLIGHT = 0
+basic.showString(control.deviceName())
 GREENLIGHT = 1
 REDLIGHT = 2
 radio.setGroup(1)
