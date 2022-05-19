58058dc7f69fcf1535b319a8b347e88b6c3600d3
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-19 02:40:17 +0200

    receive player IDs

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    list = []
+    players = []
     state = NEWGAME
     basic.pause(1000)
     music.playMelody("D - D - D - C5 C5 ", 100)
+    basic.showNumber(players.length)
+    basic.pause(1000)
 })
 input.onButtonPressed(Button.A, function () {
     state = GREENLIGHT
@@ -13,8 +15,18 @@ input.onButtonPressed(Button.B, function () {
     state = REDLIGHT
     basic.showIcon(IconNames.No)
 })
+radio.onReceivedValue(function (name, value) {
+    basic.showString(name)
+    basic.showString("" + (value))
+    if (name == "new") {
+        players.push(value)
+    } else if (name == "eliminated") {
+        players.removeAt(players.indexOf(value))
+        basic.showNumber(players.length)
+    }
+})
 let state = 0
-let list: number[] = []
+let players: number[] = []
 let REDLIGHT = 0
 let GREENLIGHT = 0
 let NEWGAME = 0
@@ -24,7 +36,7 @@ GREENLIGHT = 1
 REDLIGHT = 2
 radio.setGroup(1)
 radio.setTransmitSerialNumber(true)
-list = []
+players = []
 basic.forever(function () {
     radio.sendNumber(state)
 })
