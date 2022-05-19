32934045d6b787bcc6e06e70438c1a3a7362fdb9
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-19 02:22:26 +0200

    Send radio ID, for initial phase and for game-over

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
-let movement = 0
-let REDLIGHT = 0
-let state = 0
-let GREENLIGHT = 0
 radio.onReceivedNumber(function (receivedNumber) {
     state = receivedNumber
 })
-GREENLIGHT = 1
-REDLIGHT = 2
+let movement = 0
+let state = 0
+let NEWGAME = 0
+let GREENLIGHT = 1
+let REDLIGHT = 2
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(3)
 radio.setGroup(1)
 basic.forever(function () {
     if (state == GREENLIGHT) {
         basic.showIcon(IconNames.Yes)
     } else if (state == REDLIGHT) {
         basic.showIcon(IconNames.No)
-    }
-})
-basic.forever(function () {
-    if (state == REDLIGHT) {
         movement = Math.abs(input.acceleration(Dimension.Strength) - 1000)
-        if (movement != 0) {
+        if (movement >= 250) {
             game.gameOver()
+            radio.sendValue("eliminated", control.deviceSerialNumber())
         }
+    } else if (state == NEWGAME) {
+        radio.sendValue("new", control.deviceSerialNumber())
     }
 })
