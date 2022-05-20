a9da0ad3185a1abd913024e90b5eed10c44fc763
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-20 09:17:47 +0200

    refactor messaging, life counter, winner, eliminated state

--- main.ts
+++ main.ts
@@ -1,25 +1,45 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    state = receivedNumber
+radio.onReceivedValue(function (name, value) {
+    if (name == "winner") {
+        music.stopAllSounds()
+        if (value == control.deviceSerialNumber()) {
+            basic.showIcon(IconNames.Heart)
+            music.playMelody("F G A B B - B C5 ", 120)
+            music.stopAllSounds()
+        }
+    }
+    if (name == "state") {
+        state = value
+        if (state == GREENLIGHT) {
+            basic.showIcon(IconNames.Yes)
+        } else if (state == REDLIGHT) {
+            basic.showIcon(IconNames.No)
+        } else if (state == NEWGAME) {
+            radio.sendValue("new", control.deviceSerialNumber())
+            basic.showIcon(IconNames.Heart)
+            game.addLife(1)
+        }
+    }
 })
-let movement = 0
+let rychlost = 0
 let state = 0
+let REDLIGHT = 0
+let GREENLIGHT = 0
 let NEWGAME = 0
-let GREENLIGHT = 1
-let REDLIGHT = 2
+NEWGAME = 0
+GREENLIGHT = 1
+REDLIGHT = 2
 radio.setTransmitSerialNumber(true)
 radio.setTransmitPower(3)
 radio.setGroup(1)
+game.setLife(0)
 basic.forever(function () {
-    if (state == GREENLIGHT) {
-        basic.showIcon(IconNames.Yes)
-    } else if (state == REDLIGHT) {
-        basic.showIcon(IconNames.No)
-        movement = Math.abs(input.acceleration(Dimension.Strength) - 1000)
-        if (movement >= 250) {
-            game.gameOver()
-            radio.sendValue("eliminated", control.deviceSerialNumber())
-        }
-    } else if (state == NEWGAME) {
-        radio.sendValue("new", control.deviceSerialNumber())
+    rychlost = Math.abs(input.acceleration(Dimension.Strength) - 1000)
+    if (state == REDLIGHT && rychlost >= 250) {
+        radio.sendValue("eliminated", control.deviceSerialNumber())
+        basic.showIcon(IconNames.Skull)
+        game.removeLife(1)
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        game.gameOver()
+        music.stopAllSounds()
     }
 })
