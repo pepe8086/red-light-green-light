3ca0a18fcda347e7c0a0fbe44bc975c07f5526bc
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-20 12:52:12 +0200

    [fix] adding new players

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     players = []
     state = NEWGAME
-    basic.pause(1000)
+    sendState("state", state)
     music.playMelody("D - D - D - C5 C5 ", 100)
-    basic.showNumber(players.length)
     basic.pause(1000)
+    music.stopAllSounds()
+    basic.showNumber(players.length)
 })
 input.onButtonPressed(Button.A, function () {
     state = GREENLIGHT
+    sendState("state", state)
     basic.showIcon(IconNames.Yes)
-    music.playMelody("E C D C E D C D ", 329)
+    music.playMelody("E C D C E D C D ", 323)
+    music.stopAllSounds()
 })
 function debugMessage (name: string, value: number) {
     basic.showString(name)
@@ -17,31 +20,39 @@ function debugMessage (name: string, value: number) {
 }
 input.onButtonPressed(Button.B, function () {
     state = REDLIGHT
+    sendState("state", state)
     basic.showIcon(IconNames.No)
-    music.playMelody("E C D C E D C D ", 40)
-})
-radio.onReceivedValue(function (name, value) {
-    debugMessage(name, value)
-    if (name == "new" && state == 0) {
-        players.push(value)
-    } else if (name == "new" && state == 2) {
-        players.removeAt(players.indexOf(value))
-    }
+    music.playMelody("E C D C E D C D ", 42)
+    basic.pause(2000)
+    music.stopAllSounds()
     basic.showNumber(players.length)
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "new" && state == NEWGAME) {
+        if (players.indexOf(value) == -1) {
+            players.push(value)
+        }
+    } else if (name == "eliminated") {
+        basic.showString(name)
+        players.removeAt(players.indexOf(value))
+        basic.showNumber(players.length)
+    }
+})
+function sendState (text: string, num: number) {
+    radio.sendValue(text, num)
+}
 let players: number[] = []
 let state = 0
+let NEWGAME = 0
 let REDLIGHT = 0
 let GREENLIGHT = 0
-let NEWGAME = 0
-NEWGAME = 0
 GREENLIGHT = 1
 REDLIGHT = 2
-state = NEWGAME
+NEWGAME = 3
+let PATIENCE = 4
+state = PATIENCE
 radio.setGroup(1)
 radio.setTransmitSerialNumber(true)
 players = []
-basic.forever(function () {
-    radio.sendValue("state", state)
-    basic.pause(100)
-})
+music.playTone(262, music.beat(BeatFraction.Whole))
+basic.showIcon(IconNames.Ghost)
