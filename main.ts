8b581e90aea403a4a7824dbcf080e01b9fcab47c
pepe8086 <pepe8086@users.noreply.github.com>
2022-05-20 12:54:06 +0200

    [fix] add new players

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ radio.onReceivedValue(function (name, value) {
     if (name == "winner") {
         music.stopAllSounds()
         if (value == control.deviceSerialNumber()) {
-            basic.showIcon(IconNames.Heart)
+            basic.showIcon(IconNames.Happy)
             music.playMelody("F G A B B - B C5 ", 120)
             music.stopAllSounds()
         }
@@ -14,32 +14,37 @@ radio.onReceivedValue(function (name, value) {
         } else if (state == REDLIGHT) {
             basic.showIcon(IconNames.No)
         } else if (state == NEWGAME) {
-            radio.sendValue("new", control.deviceSerialNumber())
             basic.showIcon(IconNames.Heart)
             game.addLife(1)
+            radio.sendValue("new", control.deviceSerialNumber())
+            basic.pause(randint(10, 500))
         }
     }
 })
 let rychlost = 0
 let state = 0
+let NEWGAME = 0
 let REDLIGHT = 0
 let GREENLIGHT = 0
-let NEWGAME = 0
-NEWGAME = 0
 GREENLIGHT = 1
 REDLIGHT = 2
+NEWGAME = 3
+let PATIENCE = 4
 radio.setTransmitSerialNumber(true)
 radio.setTransmitPower(3)
 radio.setGroup(1)
-game.setLife(0)
+state = PATIENCE
+basic.showIcon(IconNames.House)
 basic.forever(function () {
     rychlost = Math.abs(input.acceleration(Dimension.Strength) - 1000)
     if (state == REDLIGHT && rychlost >= 250) {
         radio.sendValue("eliminated", control.deviceSerialNumber())
+        state = PATIENCE
         basic.showIcon(IconNames.Skull)
-        game.removeLife(1)
+        basic.showIcon(IconNames.Skull)
+        basic.showIcon(IconNames.Skull)
         music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-        game.gameOver()
+        game.removeLife(1)
         music.stopAllSounds()
     }
 })
